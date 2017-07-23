17d510ee1b71200c0152d427fd43ab22a60b38b4
Lean <lean@LeandeMacBook-Pro.local>
2017-07-23 17:08:40 +0800

    Initial Commit

---
+++ BusinessManagementSystem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BusinessManagementSystem
+//
+//  Created by Lean on 2017/7/23.
+//  Copyright © 2017年 Lean. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
