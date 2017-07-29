e27dbf4fd35619896ed14e1bb8a18f379e31e865
InvoKer <1005890623@qq.com>
2017-07-29 17:14:32 +0800

    ~~

--- BusinessManagementSystem/Controller.cpp
+++ BusinessManagementSystem/Controller.cpp
@@ -10,12 +10,32 @@
 #include "EventController.h"
 #include "CatalogueController.h"
 #include "Data.h"
+#include "Test_option.hpp"
+
+#import "sqlite3.h"
+
 
 #include <iostream>
 
 using namespace std;
 
+void Initialize_DataBase(sqlite3 *Database, char *err_msg)
+{
+    char Sql_Create_Buffer[200]= "create table event(Id integer primary key,Title text,Catalogue text,Detail text,State integer,Start_Time text,End_Time text)";
+    CreateTable_Manual(Database, err_msg, Sql_Create_Buffer);
+}
 
+void Set_To_DataBase()
+{
+    
+}
+
+void Insert_Call_Test(sqlite3 *Database, char *err_msg){
+    char Sql_Insert_Buffer[200] = "insert into event values (54,'车日天是狗','全部事项',0,'201707272330','201707272400')";
+    Sql_Manual(Database, err_msg, Sql_Insert_Buffer);
+}
+
+void Get_From_DataBase();
 
 
 
---
+++ BusinessManagementSystem/Database_controll_API.hpp
@@ -0,0 +1,50 @@
+//
+//  Database_controll_API.hpp
+//  DataBaseTest
+//
+//  Created by 龙子俊 on 2017/7/27.
+//  Copyright © 2017年 Longkuku. All rights reserved.
+//
+
+#ifndef Database_controll_API_hpp
+#define Database_controll_API_hpp
+
+#include <stdio.h>
+#import "sqlite3.h"
+#include <iostream>
+using namespace std;
+
+inline void OpenDataBase(sqlite3 *Database, char *err_msg){if(sqlite3_open("test.db", &Database) != SQLITE_OK)
+{
+    printf("无法打开，错误代码: %s\n", sqlite3_errmsg(Database));
+    exit(-1);
+}
+else printf("打开数据库成功！\n");
+}
+
+inline void CloseDataBase(sqlite3 *Database, char *err_msg){ if (sqlite3_close(Database) != SQLITE_OK)
+{
+    printf("无法关闭，错误代码: %s\n", sqlite3_errmsg(Database));
+    exit(-1);
+}
+    else printf("关闭数据库成功！\n");}
+
+int sqlite3_exec_callback(void *data, int nColumn,char **colValues, char **colNames);
+
+int sqlite3_exec_callback_checkexsit(void *data, int nColumn, char **colValues, char **colNames);
+
+int CheckTableExist(char *Sql,sqlite3 *Database, char *err_msg);
+
+
+
+
+
+void CreateTable_Manual(sqlite3 *Database, char *err_msg, char *sql);
+
+
+void Sql_Manual(sqlite3 *Database, char *err_msg,char *sql);
+
+
+void Displaytable_Manual(sqlite3 *Database, char *err_msg,char *sql);
+
+#endif /* Database_controll_API_hpp */
